60b55d6e4d9e30ad73b83b7d0ae588aec132ba34
sevshum <dev@gmail.com>
2016-10-05 15:47:15 +0300

    init

---
+++ config/params.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+//    'kladrToken' => '57f4ef7b0a69de3f168b4579',
+    'kladrToken' => '51dfe5d42fb2b43e3300006e',
+    'kladrKey' => '86a2c2a06f1b2451a87d05512cc2c3edfdf41969',
+];
